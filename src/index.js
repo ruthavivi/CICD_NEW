03050009580d7e67b688efe1948b4e8e585626b8
ruthavivi <73632861+ruthavivi@users.noreply.github.com>
2022-03-13 23:42:53 +0200

    cicd class 1

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const port = process.env.PORT || 4000;
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('<h1>from server main page<h1>');
+});
+
+app.listen(port,()=> {
+    console.log('server is up and running');
+});
