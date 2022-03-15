421ea0bdb719efa740f5b047821c720de0173d17
ruthavivi <73632861+ruthavivi@users.noreply.github.com>
2022-03-15 12:14:34 +0200

    all

--- tests/Arrays.spec.js
+++ tests/Arrays.spec.js
@@ -4,7 +4,7 @@ describe('Array',()=>{
     describe('#sort',()=>{
         it('should sort the Array by name',()=>{
             var names=['dani','moshe','adam'];
-            expect(names.sort()).toLocaleString.be.eql(['adam','dani','moshe']);
+            expect(names.sort()).to.be.eql(['adam','dani','moshe']);
         });
     });
 });
